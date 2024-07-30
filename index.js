544810d30926aa97baa09b6763083ddeaf51f61e
riyanshu11 <palriyanshu@gmail.com>
2024-07-30 11:56:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("sidebar").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("sidebar").style.display = "none";
+}
+
+function openPage(){
+ document.getElementsById("pagecontent").style.display="block";
+}
